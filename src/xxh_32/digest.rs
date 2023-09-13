1d31e7b222b02cf0e0ef215239bac795d1b505fc
David James <davidcjames@gmail.com>
2023-09-13 09:30:52 -0400

    minor refactor

--- src/xxh_32/digest.rs
+++ src/xxh_32/digest.rs
@@ -11,20 +11,18 @@ pub fn digest(data: &[u8], seed: u32) -> u32 {
 
 /// Returns the XXH_32 digest for less than 16 bytes of data.
 pub fn digest_short(bytes: &[u8], seed: u32) -> u32 {
-    let length = bytes.len();
-    debug_assert!(length < 16);
+    debug_assert!(bytes.len() < 16);
     let mut a1 = Acc1::new(seed);
-    a1.finish(length, bytes)
+    a1.finish(bytes.len(), bytes)
 }
 
 /// Returns the XXH_32 digest for 16 bytes of data or more. (In XXH_32, a stripe
 /// has exactly 16 bytes.)
 pub fn digest_long(bytes: &[u8], seed: u32) -> u32 {
-    let length = bytes.len();
-    debug_assert!(length >= 16);
+    debug_assert!(bytes.len() >= 16);
     let mut a4 = Acc4::new(seed);
     let mut chunks = bytes.chunks_exact(16);
     a4.consume_stripes(chunks.by_ref());
     let mut a1 = a4.converge();
-    a1.finish(length, chunks.remainder())
+    a1.finish(bytes.len(), chunks.remainder())
 }
